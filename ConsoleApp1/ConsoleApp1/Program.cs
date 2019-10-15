ba32946bb88caebfe235384973f2d11e59100b5c
Alejandroid101 <kik.rdl3@gmail.com>
2019-10-15 15:35:15 -0700

    StartSequence method (mostly) done

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the numbers game!");
+            Console.WriteLine("Hello World!");
+            StartSequence();
+        }
+
+        public static void StartSequence()
+        {
+            Console.Write("Enter a number greater than 0!");
+
+            int userNumber;
+            userNumber = Convert.ToInt32(Console.ReadLine());
+            int[] newArr = new int[userNumber];
+
+            //Start calling the Methods and capture their values
+            Populate(newArr);
+            int sum = GetSum(newArr);
+            int product = GetProduct(newArr, sum);
+            int quotient = GetQuotient(product);
+            Console.WriteLine($"Your array size is {userNumber}");
+            Console.WriteLine($"The numbers in the Array are: ", string.Join(", ", newArr));
+            Console.WriteLine(sum);
+            Console.WriteLine(product);
+            Console.WriteLine(quotient);
+
+        }
+
+        public static int[] Populate(int[] arr)
+        {
+
+        }
+    }
+}
