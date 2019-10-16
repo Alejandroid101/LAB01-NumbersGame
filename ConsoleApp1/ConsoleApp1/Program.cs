d11cae9aec379d884159d257df52f8cfe56b6f15
Alejandroid101 <kik.rdl3@gmail.com>
2019-10-15 17:20:41 -0700

    Populate method complete!

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,10 +14,10 @@ namespace ConsoleApp1
         public static void StartSequence()
         {
             Console.Write("Enter a number greater than 0!");
+            string userNumber = Console.ReadLine();
+            int usIn = Convert.ToInt32(userNumber);
 
-            int userNumber;
-            userNumber = Convert.ToInt32(Console.ReadLine());
-            int[] newArr = new int[userNumber];
+            int[] newArr = new int[usIn];
 
             //Start calling the Methods and capture their values
             Populate(newArr);
@@ -34,7 +34,13 @@ namespace ConsoleApp1
 
         public static int[] Populate(int[] arr)
         {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"Please enter a number {i + 1}/{arr.Length}: ");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
 
+            }
+            return arr;
         }
     }
 }
